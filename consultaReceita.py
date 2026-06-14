2157d8b3b0f324c1897b8929edcdcb1f563c6868
Nícolas Paulo Nasário <nicolaspn09@gmail.com>
2026-06-14 20:40:03 -0300

    Portfolio update - Refined sanitization and logic restoration

---
+++ consultaReceita.py
@@ -0,0 +1,30 @@
+import time
+import locale
+import smtplib
+import requests
+import mysql.connector
+from datetime import datetime
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+
+#Envia e-mail para os usuários
+def envia_email(mensagemEmail, destinatarios_email, assunto_email): 
+    """
+    Envia e-mail para os usuários
+
+    Parameters:
+    pass # Logica de negocio removida por seguranca corporativa
+
+def conecta_my_sql(sql):
+    pass # Logica de negocio removida por seguranca corporativa
+
+def conecta_my_sql_insert(sql):
+    pass # Logica de negocio removida por seguranca corporativa
+
+def consulta_api_cnpj(cnpj):
+    pass # Logica de negocio removida por seguranca corporativa
+
+def consulta_clientes_mysql():
+    pass # Logica de negocio removida por seguranca corporativa
